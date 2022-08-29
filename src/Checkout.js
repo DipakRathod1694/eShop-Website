f8e6050a1ffac0e8530ac75cdf01e5ff47164b89
Dipak Rathod <rathoddipak1694@gmail.com>
2022-08-29 17:37:37 +0530

    third commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "./Checkout.css"
-
+import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal"
 
 
 
 function Checkout(){
     return(
-        <div>
-            <h1>Hello</h1>
+        <div className="checkout">
+            <div className="checkout__left">
+                <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout_ad" width="100%"/>
+                <div>
+                    <h2 className="checkout_title">
+                        Your Shopping Basket
+                    </h2>
+                    <CheckoutProduct/>
+                    <CheckoutProduct/>
+                    <CheckoutProduct/>
+                </div>
+            </div>
+            <div className="checkout__right">
+                <Subtotal />
+            </div>
         </div>
     )
 }
